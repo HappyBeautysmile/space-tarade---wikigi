cff306a33818992a432a713628da3bda8635d049
Nikita <nikita1923666@gmail.com>
2019-11-12 23:12:02 -0800

    search window work

--- tradespaceui/src/views/Search.vue
+++ tradespaceui/src/views/Search.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
-        <b-row style="text-align: center; margin: 250px 50px 0 50px">
-            <p>hi</p>
+        <b-row style="text-align: center; margin: 100px 50px 0 50px">
+            <div id="cover">
+                <form method="get" action="">
+                    <div class="tb">
+                        <div class="td">
+                            <input type="text" placeholder="Search" required>
+                        </div>
+                        <div class="td" id="s-cover">
+                            <button type="submit">
+                                <div id="s-circle"></div>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </b-row>
     </div>
 </template>
@@ -13,51 +27,160 @@
 </script>
 
 <style scoped>
-    body {
-        background: #f2f2f2;
-        font-family: 'Open Sans', sans-serif;
+
+    body
+    {
+        margin: 0;
+        background-color: #ffd8d8;
     }
 
-    .search {
+    .tb
+    {
+        display: table;
         width: 100%;
-        position: relative;
-        display: flex;
-        /*height: 50px;*/
     }
 
-    .searchTerm {
-        width: 100%;
-        border: 3px solid #00B4CC;
-        border-right: none;
-        padding: 5px;
-        height: 20px;
-        border-radius: 5px 0 0 5px;
-        outline: none;
-        color: #9DBFAF;
+    .td
+    {
+        display: table-cell;
+        vertical-align: middle;
     }
 
-    .searchTerm:focus {
-        color: #00B4CC;
-    }
-
-    .searchButton {
-        width: 40px;
-        height: 36px;
-        border: 1px solid #00B4CC;
-        background: #00B4CC;
-        text-align: center;
+    input, button
+    {
         color: #fff;
-        border-radius: 0 5px 5px 0;
-        cursor: pointer;
-        font-size: 20px;
+        font-family: Nunito;
+        padding: 0;
+        margin: 0;
+        border: 0;
+        background-color: transparent;
     }
 
-    /*Resize the wrap to see the search bar change!*/
-    .wrap {
-        width: 30%;
+    #cover
+    {
         position: absolute;
         top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        left: 0;
+        right: 0;
+        width: 100%;
+        padding: 5px;
+        margin: -83px auto 0 auto;
+        background-color: #a4a0a9;
+        border-radius: 20px;
+        box-shadow: 0 10px 40px #a4a0a9, 0 0 0 20px #ffffffeb;
+        transform: scale(0.6);
+    }
+
+    form
+    {
+        height: 96px;
+    }
+
+    input[type="text"]
+    {
+        width: 100%;
+        height: 96px;
+        font-size: 60px;
+        line-height: 1;
+    }
+
+    input[type="text"]::placeholder
+    {
+        color: #000000;
+    }
+
+    #s-cover
+    {
+        width: 1px;
+        padding-left: 35px;
+    }
+
+    button
+    {
+        position: relative;
+        display: block;
+        width: 84px;
+        height: 96px;
+        cursor: pointer;
+    }
+
+    #s-circle
+    {
+        position: relative;
+        top: -8px;
+        left: 0;
+        width: 43px;
+        height: 43px;
+        margin-top: 0;
+        border-width: 15px;
+        border: 15px solid #fff;
+        background-color: transparent;
+        border-radius: 50%;
+        transition: 0.5s ease all;
+    }
+
+    button span
+    {
+        position: absolute;
+        top: 68px;
+        left: 43px;
+        display: block;
+        width: 45px;
+        height: 15px;
+        background-color: transparent;
+        border-radius: 10px;
+        transform: rotateZ(52deg);
+        transition: 0.5s ease all;
+    }
+
+    button span:before, button span:after
+    {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 45px;
+        height: 15px;
+        background-color: #fff;
+        border-radius: 10px;
+        transform: rotateZ(0);
+        transition: 0.5s ease all;
+    }
+
+    #s-cover:hover #s-circle
+    {
+        top: -1px;
+        width: 67px;
+        height: 15px;
+        border-width: 0;
+        background-color: #fff;
+        border-radius: 20px;
+    }
+
+    #s-cover:hover span
+    {
+        top: 50%;
+        left: 56px;
+        width: 25px;
+        margin-top: -9px;
+        transform: rotateZ(0);
+    }
+
+    #s-cover:hover button span:before
+    {
+        bottom: 11px;
+        transform: rotateZ(52deg);
+    }
+
+    #s-cover:hover button span:after
+    {
+        bottom: -11px;
+        transform: rotateZ(-52deg);
+    }
+    #s-cover:hover button span:before, #s-cover:hover button span:after
+    {
+        right: -6px;
+        width: 40px;
+        background-color: #fff;
     }
 </style>
