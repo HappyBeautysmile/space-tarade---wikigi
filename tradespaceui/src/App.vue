1ef9e0379f28e49a3024052b4f5d4e50794c94d2
Nikita <nikita1923666@gmail.com>
2019-11-13 01:50:49 -0800

    search finished

--- tradespaceui/src/App.vue
+++ tradespaceui/src/App.vue
@@ -2,14 +2,16 @@
     <v-app style="margin: 0; padding: 0">
         <v-app-bar app>
             <v-toolbar-title class="headline text-uppercase">
-                <img style="padding: 0; margin: 0" :src="require('@/assets/logo3.png')" height="48" width="50"/>
-                <span>Trade</span>
-                <span class="font-weight-light">Space</span>
+                <router-link to="/" style="text-decoration:none;">
+                    <img style="padding: 0; margin: 0" :src="require('@/assets/logo3.png')" height="48" width="50"/>
+                    <span style="text-decoration:none; color: black">Trade</span>
+                    <span class="font-weight-light" style="text-decoration:none; color: black">Space</span>
+                </router-link>
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <router-link to="/search">
                 <v-btn text>
-                    <span class="mr-2">For trade</span>
+                    <span class="mr-2">Search</span>
                 </v-btn>
             </router-link>
             <router-link to="/account">
@@ -24,7 +26,7 @@
             </router-link>
         </v-app-bar>
         <v-content style="margin: 0; padding: 0">
-<!--            <HelloWorld/>-->
+            <!--            <HelloWorld/>-->
             <router-view></router-view>
         </v-content>
     </v-app>
