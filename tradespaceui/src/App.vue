47d1d77a2c669b3f16a25ed4631e0cbbed50c7b6
DarkBear0121 <darkbear0112@gmail.com>
2019-11-13 00:13:30 -0800

    made TradeSpace on app bar a hyperlink and changed account ui

--- tradespaceui/src/App.vue
+++ tradespaceui/src/App.vue
@@ -1,45 +1,52 @@
 <template>
-    <v-app style="margin: 0; padding: 0">
-        <v-app-bar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <img style="padding: 0; margin: 0" :src="require('@/assets/logo3.png')" height="48" width="50"/>
-                <span>Trade</span>
-                <span class="font-weight-light">Space</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <router-link to="/search">
-                <v-btn text>
-                    <span class="mr-2">For trade</span>
-                </v-btn>
-            </router-link>
-            <router-link to="/account">
-                <v-btn text>
-                    <span class="mr-2">Account</span>
-                </v-btn>
-            </router-link>
-            <router-link to="/createItem">
-                <v-btn text>
-                    <span class="mr-2">Start Trade</span>
-                </v-btn>
-            </router-link>
-        </v-app-bar>
-        <v-content style="margin: 0; padding: 0">
-<!--            <HelloWorld/>-->
-            <router-view></router-view>
-        </v-content>
-    </v-app>
+  <v-app style="margin: 0; padding: 0">
+    <v-app-bar app>
+      <router-link to="/" style="text-decoration: none; color: black;">
+        <v-toolbar-title class="headline text-uppercase">
+          <img
+            style="padding: 0; margin: 0"
+            :src="require('@/assets/logo3.png')"
+            height="48"
+            width="50"
+          />
+          <span>Trade</span>
+          <span class="font-weight-light">Space</span>
+        </v-toolbar-title>
+      </router-link>
+      <v-spacer></v-spacer>
+      <router-link to="/search">
+        <v-btn text>
+          <span class="mr-2">For trade</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/account">
+        <v-btn text>
+          <span class="mr-2">Account</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/createItem">
+        <v-btn text>
+          <span class="mr-2">Start Trade</span>
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+    <v-content style="margin: 0; padding: 0">
+      <!--            <HelloWorld/>-->
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
-    export default {
-        name: 'App',
-        // components: {
-        //     HelloWorld,
-        // },
-        data: () => ({
-            //
-        }),
-    };
+export default {
+  name: "App",
+  // components: {
+  //     HelloWorld,
+  // },
+  data: () => ({
+    //
+  })
+};
 </script>
