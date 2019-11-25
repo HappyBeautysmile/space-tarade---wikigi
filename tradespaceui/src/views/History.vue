73e92c779e4a29e80f33b2deb2837b7808c59dda
DarkBear0121 <darkbear0112@gmail.com>
2019-11-25 01:07:37 -0800

    Worked on my items connecting front and back end

--- tradespaceui/src/views/History.vue
+++ tradespaceui/src/views/History.vue
@@ -20,6 +20,9 @@
 <script>
 
 import Item from '../components/item';
+import firebase from 'firebase';
+import axios from 'axios';
+import qs from 'querystring';
 
 export default {
   
@@ -30,8 +33,77 @@ export default {
   data: () => ({
     items: [
       'foo', 'bar', 'bork', 'dog', 'shoe', 'catch', 'p1', 'p2'
-    ]
-  })
+    ],
+    
+  }),
+  methods: 
+  {
+    getUserItems: function () {
+        //TODO: Get items from firebase
+
+
+        let self = this;
+        axios.post('/users/', qs.stringify({
+            'email': self.email,
+            'password': self.password,
+            'display_name': self.name,
+            'phone_number': self.phone
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then(response => {
+                alert(response);
+                self.dialog2 = false;
+                self.snackbar = true;
+                self.text = 'Your account have been created';
+                self.$router.replace('home');
+                self.$store.commit('logIn', true);
+                // this.$router.go(0);
+            })
+            .catch(error => {
+                // alert(e + e.status + e.code)
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // alert("ERROR:" + errorMessage + errorCode);
+                self.snackbar = true;
+                self.text = "ERROR " + errorCode + ":" + errorMessage;
+            });
+    },
+    getItems: function () {
+        var self = this;
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+            function () {
+                self.dialog1 = false;
+                self.snackbar = true;
+                firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function (idToken) {
+                    alert("Token: " + idToken)
+                }).catch(function (error) {
+                    self.text = "ERROR:" + error;
+                });
+                self.text = 'Welcome back!';
+                self.$router.replace('home');
+                self.$store.commit('logIn', true)
+                // self.$router.go(0);
+            },
+            function (error) {
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // alert("ERROR:" + errorMessage + errorCode);
+                self.snackbar = true;
+                self.text = "ERROR " + errorCode + ":" + errorMessage;
+            }
+        );
+
+    },
+  }
+  
+
+
+
 };
 </script>
 
