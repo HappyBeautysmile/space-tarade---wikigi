ca76d40a882795dff43e26df8f32f5661a43c5f2
HirdayGupta <hirday.gupta@gmail.com>
2019-12-01 22:57:56 -0800

    Add all of trades API

--- tradespace_backend/src/TradesAPI.py
+++ tradespace_backend/src/TradesAPI.py
@@ -12,6 +12,267 @@ trades_api = Blueprint('trades_api', __name__)
 TRADES_COLLECTION = 'trades'
 ITEMS_COLLECTION = 'items'
 
+"""
+Get a single trade with the given ID. The returned object is of type of given trade.
+Responses: 200 - ok, 400 - bad request, 500 - internal error
+"""
+@trades_api.route('/<string:trade_id>', methods=['GET'])
+@auth.login_required
+def render_trade(trade_id):
+    db = firestore.client()
+    try:
+        trade_dict = db.collection(TRADES_COLLECTION).document(trade_id).get().to_dict()
+        trade_object = Trade.from_dict(trade_dict)
+        if trade_object is None:
+            return {'error': 'something is wrong, we are on it.'}, 500
+    except Exception as inst:
+        print (inst)
+        return {'error': 'given trade not found'}, 404
+
+    rendered_trade = trade_object.render(g.uid)
+    if (rendered_trade is None):
+        return {"error": "something went wrong on our end. please try again later."}, 500
+
+    return rendered_trade, 200
+
+def get_trade(trade_id):
+    db = firestore.client()
+    try:
+        trade_dict = db.collection(TRADES_COLLECTION).document(trade_id).get().to_dict()
+        trade_object = Trade.from_dict(trade_dict)
+        if trade_object is None:
+            return {'error': 'something is wrong, we are on it.'}, 500
+    except Exception as inst:
+        print (inst)
+        return {'error': 'given trade not found'}, 404
+
+    return trade_object.to_dict(), 200
+
+"""
+Called when the buyer clicks "request trade" on the user's item `itemId`.
+Creates a trade with type "none" and basic info seller & buyer UIDs and
+selling_item ID.
+Response codes: 201 - successfully created, 400 - bad request, 500 - internal error
+"""
+@trades_api.route('/request_new', methods=['POST'])
+@auth.login_required
+def create_trade():
+    try:
+        selling_item_id = request.form['item_id']
+    except:
+        return {'error': 'missing required params'}, 400
+
+    response, response_code = get_item(selling_item_id)
+    if response_code != 200:
+        return response, response_code
+
+    item = Item.from_dict(response)
+    if (item.owner_uid == g.uid):
+        return {'error': 'cannot request trade with your own item'}, 400
+
+    # TODO: add check to make sure user hasn't already requested trade with this item.
+
+    db = firestore.client()
+
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document()
+    except:
+        return {'error': 'could not create new trade, try again later'}, 500
+
+    new_trade = Trade(new_trade_ref.id, g.uid, item.owner_uid, selling_item_id)
+
+    try:
+        new_trade_ref.set(new_trade.to_dict())
+    except:
+        return {'error': 'could not push new trade, try again later'}, 500
+
+    return new_trade.to_dict(), 201
+
+@trades_api.route('/<string:trade_id>/barter', methods=['PUT'])
+@auth.login_required
+def make_barter_trade(trade_id):
+    # param validation
+    try:
+        buying_item_id = request.form['buyer_item']
+    except:
+        return {'error': 'missing required params'}, 400
+
+    # make sure trade exists
+    response, response_code = get_trade(trade_id)
+    if response_code != 200:
+        return response, response_code
+
+    trade_dict = response
+
+    # ensure provided item ID exsists
+    response, response_code = get_item(buying_item_id)
+    if response_code != 200:
+        return response, response_code
+
+    item_dict = response
+
+    # ensure trade buyer owns given item
+    if (item_dict['owner_uid'] != trade_dict['buyer_id']):
+        return {'error': "this trade's buyer doesn't own the given item"}, 400
+
+    trade_obj = Trade.from_dict(trade_dict)
+
+    if (trade_obj.compute_status(g.uid) != 1):
+        return {'error': "this trade's status is not 1 and hence cannot be updated with this method."}, 400
+
+    trade_dict['buyer_item'] = buying_item_id
+    new_trade_obj = BarterTrade.from_dict(trade_dict)
+
+    db = firestore.client()
+
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document(trade_id)
+        new_trade_ref.update(new_trade_obj.to_dict())
+    except:
+        return {'error': 'could not update trade, try again later'}, 500
+
+    return new_trade_obj.to_dict(), 200
+
+
+@trades_api.route('/<string:trade_id>/money', methods=['PUT'])
+@auth.login_required
+def make_money_trade(trade_id):
+    # param validation
+    try:
+        buyer_price = request.form['buyer_price']
+    except:
+        return {'error': 'missing required params'}, 400
+
+    # make sure trade exists
+    response, response_code = get_trade(trade_id)
+    if response_code != 200:
+        return response, response_code
+
+    trade_dict = response
+    trade_obj = Trade.from_dict(trade_dict)
+
+    if (trade_obj.compute_status(g.uid) != 1):
+        return {'error': "this trade's status is not 1 and hence cannot be updated with this method."}, 400
+
+    trade_dict['buyer_price'] = buyer_price
+    new_trade_obj = MoneyTrade.from_dict(trade_dict)
+
+    db = firestore.client()
+
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document(trade_id)
+        new_trade_ref.update(new_trade_obj.to_dict())
+    except:
+        return {'error': 'could not update trade, try again later'}, 500
+
+    return new_trade_obj.to_dict(), 200
+
+@trades_api.route('/<string:trade_id>/barter_and_money', methods=['PUT'])
+@auth.login_required
+def make_barter_and_money_trade(trade_id):
+    # param validation
+    try:
+        buying_item_id = request.form['buyer_item']
+        buyer_price = request.form['buyer_price']
+    except:
+        return {'error': 'missing required params'}, 400
+
+    # make sure trade exists
+    response, response_code = get_trade(trade_id)
+    if response_code != 200:
+        return response, response_code
+
+    trade_dict = response
+
+    # ensure provided item ID exsists
+    response, response_code = get_item(buying_item_id)
+    if response_code != 200:
+        return response, response_code
+
+    item_dict = response
+
+    # ensure trade buyer owns given item
+    if (item_dict['owner_uid'] != trade_dict['buyer_id']):
+        return {'error': "this trade's buyer doesn't own the given item"}, 400
+
+    trade_obj = Trade.from_dict(trade_dict)
+    # ensure trade's status allows it to be updated like this
+    if (trade_obj.compute_status(g.uid) != 1):
+        return {'error': "this trade's status is not 1 and hence cannot be updated with this method."}, 400
+
+    trade_dict['buyer_item'] = buying_item_id
+    trade_dict['buyer_price'] = buyer_price
+    new_trade_obj = BarterAndMoneyTrade.from_dict(trade_dict)
+
+    db = firestore.client()
+
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document(trade_id)
+        new_trade_ref.update(new_trade_obj.to_dict())
+    except:
+        return {'error': 'could not update trade, try again later'}, 500
+
+    return new_trade_obj.to_dict(), 200
+
+@trades_api.route('/<string:trade_id>/complete', methods=['POST'])
+@auth.login_required
+def complete_trade(trade_id):
+    # make sure trade exists
+    response, response_code = get_trade(trade_id)
+    if response_code != 200:
+        return response, response_code
+
+    # ensure trade's status is valid for this method
+    trade_dict = response
+    trade_obj = Trade.from_dict(trade_dict)
+    if (trade_obj.compute_status(g.uid) != 2):
+        return {'error': "this trade is not ready to be completed."}, 400
+
+    # ensure user is either buyer or seller for this trade
+    if (trade_dict['seller_id'] != g.uid and trade_dict['buyer_id'] != g.uid):
+        return {'error': "this user is not the seller for this trade and is unathorized to perform this action"}, 401
+
+    trade_obj.mark_completed()
+
+    db = firestore.client()
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document(trade_id)
+        new_trade_ref.update(trade_obj.to_dict())
+    except:
+        return {'error': 'could not update trade status, try again later'}, 500
+
+    return trade_obj.to_dict(), 200
+
+@trades_api.route('/<string:trade_id>/remove', methods=['POST'])
+@auth.login_required
+def remove_trade(trade_id):
+    # make sure trade exists
+    response, response_code = get_trade(trade_id)
+    if response_code != 200:
+        return response, response_code
+
+    # ensure trade's status is valid for this method
+    trade_dict = response
+    trade_obj = Trade.from_dict(trade_dict)
+    if (trade_obj.compute_status(g.uid) != 2):
+        return {'error': "this trade is not ready to be cancelled."}, 400
+
+    # ensure user is either buyer or seller for this trade
+    if (trade_dict['seller_id'] != g.uid and trade_dict['buyer_id'] != g.uid):
+        return {'error': "this user is not the seller for this trade and is unathorized to perform this action"}, 401
+
+    trade_obj.mark_cancelled()
+
+    db = firestore.client()
+    try:
+        new_trade_ref = db.collection(TRADES_COLLECTION).document(trade_id)
+        new_trade_ref.update(trade_obj.to_dict())
+    except:
+        return {'error': 'could not update trade status, try again later'}, 500
+
+    return trade_obj.to_dict(), 200
+
 @trades_api.route('/', methods=['GET'])
-def get_trade():
-  return {'trades': 'api'}, 200
+@auth.login_required
+def get_trades(trade_id):
+    pass
--- tradespace_backend/src/models/Trades.py
+++ tradespace_backend/src/models/Trades.py
@@ -1,86 +1,219 @@
+from src.UsersAPI import get_user_with_id
+from src.ItemsAPI import get_item
+
 class Trade:
-    def __init__(self, buyer_id, seller_id, seller_item):
+    def __init__(self, id, buyer_id, seller_id, seller_item):
         self.buyer_id = buyer_id
         self.seller_id = seller_id
         self.seller_item = seller_item
+        self.id = id
         self.type = None
+        self.status = "PENDING"
 
     @staticmethod
     def from_dict(source):
-        buyer_id = source.get('buyer_id')
-        seller_id = source.get('seller_id')
         type = source.get('type')
-        seller_item = source.get('seller_item')
-        return Trade(buyer_id, seller_id, type, seller_item)
+        if (type == None):
+            id = source.get('id')
+            buyer_id = source.get('buyer_id')
+            seller_id = source.get('seller_id')
+            seller_item = source.get('seller_item')
+            return Trade(id, buyer_id, seller_id, seller_item)
+        elif type == "BARTER":
+            return BarterTrade.from_dict(source)
+        elif type == "MONEY":
+            return MoneyTrade.from_dict(source)
+        elif type == "BARTER_AND_MONEY":
+            return BarterAndMoneyTrade.from_dict(source)
+        else:
+            return None
 
     def to_dict(self):
         return {
+            'id': self.id,
             'buyer_id': self.buyer_id,
             'seller_id': self.seller_id,
             'type': self.type,
-            'seller_item': self.seller_item
+            'seller_item': self.seller_item,
+            'status': self.status
         }
 
+    def compute_status(self, curr_uid):
+        if (self.status ==  "PENDING"):
+            if self.type is None:
+                if self.buyer_id == curr_uid:
+                    return 0
+                elif self.seller_id == curr_uid:
+                    return 1
+            else:
+                return 2
+        elif (self.status == "COMPLETED"):
+            return 3
+        elif (self.status == "CANCELLED"):
+            return 4
+        else:
+            return -1 # undefined
+
+    def mark_completed(self):
+        self.status = "COMPLETED"
+
+    def mark_cancelled(self):
+        self.status = "CANCELLED"
+
+    def render_for_buyer(self, rendered_trade):
+        response, response_code = get_item(self.seller_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['their_item'] = response
+        return True
+
+    def render_for_seller(self, rendered_trade):
+        response, response_code = get_item(self.seller_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['your_item'] = response
+        return True
+
+    def render(self, curr_uid):
+        rendered_trade = {
+            'trade_id': self.id,
+            'status': self.compute_status(curr_uid),
+            'type': self.type
+        }
+
+        if curr_uid == self.buyer_id:
+            other_user_id = self.seller_id
+            rendered_trade['requesting_user_is'] = "BUYER"
+            did_render = self.render_for_buyer(rendered_trade)
+        elif curr_uid == self.seller_id:
+            other_user_id = self.buyer_id
+            rendered_trade['requesting_user_is'] = "SELLER"
+            did_render = self.render_for_seller(rendered_trade)
+        else:
+            return None
+
+        if (not did_render):
+            return None
+
+        other_user_dict, response_code = get_user_with_id(other_user_id)
+        rendered_trade['other_user'] = other_user_dict.get_json()
+
+        return rendered_trade
+
+
 class BarterTrade(Trade):
-    def __init__(self, buyer_id, seller_id, seller_item, buyer_item):
-        super().__init__(self, buyer_id, seller_id, seller_item)
+    def __init__(self, id, buyer_id, seller_id, seller_item, buyer_item):
+        super().__init__(id, buyer_id, seller_id, seller_item)
         self.buyer_item = buyer_item
         self.type = "BARTER"
 
     @staticmethod
     def from_dict(source):
+        id = source.get('id')
         buyer_id = source.get('buyer_id')
         seller_id = source.get('seller_id')
         type = source.get('type')
         seller_item = source.get('seller_item')
         buyer_item = source.get('buyer_item')
-        return BarterTrade(buyer_id, seller_id, type, seller_item, buyer_item)
+        return BarterTrade(id, buyer_id, seller_id, seller_item, buyer_item)
 
     def to_dict(self):
         dict_ = super().to_dict()
         dict_['buyer_item'] = self.buyer_item
         return dict_
 
+    def render_for_buyer(self, rendered_trade):
+        result = super().render_for_buyer(rendered_trade)
+        if not result: return False # if super method failed
+        response, response_code = get_item(self.buyer_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['your_item'] = response
+        return True
+
+    def render_for_seller(self, rendered_trade):
+        result = super().render_for_seller(rendered_trade)
+        if not result: return False # if super method failed
+        response, response_code = get_item(self.buyer_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['their_item'] = response
+        return True
+
 
 class MoneyTrade(Trade):
-    def __init__(self, buyer_id, seller_id, seller_item, buyer_price):
-        super().__init__(self, buyer_id, seller_id, seller_item)
+    def __init__(self, id, buyer_id, seller_id, seller_item, buyer_price):
+        super().__init__(id, buyer_id, seller_id, seller_item)
         self.buyer_price = buyer_price
         self.type = "MONEY"
 
     @staticmethod
     def from_dict(source):
+        id = source.get('id')
         buyer_id = source.get('buyer_id')
         seller_id = source.get('seller_id')
         type = source.get('type')
         seller_item = source.get('seller_item')
         buyer_price = source.get('buyer_price')
-        return MoneyTrade(buyer_id, seller_id, type, seller_item, buyer_price)
+        return MoneyTrade(id, buyer_id, seller_id, seller_item, buyer_price)
 
     def to_dict(self):
         dict_ = super().to_dict()
         dict_['buyer_price'] = self.buyer_price
         return dict_
 
+    def render_for_buyer(self, rendered_trade):
+        result = super().render_for_buyer(rendered_trade)
+        if not result: return False # if super method failed
+        rendered_trade['your_price'] = self.buyer_price
+        return True
+
+    def render_for_seller(self, rendered_trade):
+        result = super().render_for_seller(rendered_trade)
+        if not result: return False # if super method failed
+        rendered_trade['their_price'] = self.buyer_price
+        return True
+
 class BarterAndMoneyTrade(Trade):
-    def __init__(self, buyer_id, seller_id, seller_item, buyer_item, buyer_price):
-        super().__init__(self, buyer_id, seller_id, seller_item)
+    def __init__(self, id, buyer_id, seller_id, seller_item, buyer_item, buyer_price):
+        super().__init__(id, buyer_id, seller_id, seller_item)
         self.buyer_item = buyer_item
         self.buyer_price = buyer_price
         self.type = "BARTER_AND_MONEY"
 
     @staticmethod
     def from_dict(source):
+        id = source.get('id')
         buyer_id = source.get('buyer_id')
         seller_id = source.get('seller_id')
         type = source.get('type')
         seller_item = source.get('seller_item')
         buyer_price = source.get('buyer_price')
         buyer_item = source.get('buyer_item')
-        return BarterAndMoneyTrade(buyer_id, seller_id, type, seller_item, buyer_price)
+        return BarterAndMoneyTrade(id, buyer_id, seller_id, seller_item, buyer_price)
 
     def to_dict(self):
         dict_ = super().to_dict()
         dict_['buyer_item'] = self.buyer_item
         dict_['buyer_price'] = self.buyer_price
         return dict_
+
+    def render_for_buyer(self, rendered_trade):
+        result = super().render_for_buyer(rendered_trade)
+        if not result: return False # if super method failed
+        response, response_code = get_item(self.buyer_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['your_item'] = response
+        rendered_trade['your_price'] = self.buyer_price
+        return True
+
+    def render_for_seller(self, rendered_trade):
+        result = super().render_for_seller(rendered_trade)
+        if not result: return False # if super method failed
+        response, response_code = get_item(self.buyer_item)
+        if (response_code != 200):
+            return False
+        rendered_trade['their_item'] = response
+        rendered_trade['their_price'] = self.buyer_price
+        return True
