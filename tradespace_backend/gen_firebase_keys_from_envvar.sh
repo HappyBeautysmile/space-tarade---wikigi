fdb21a122ec8da64a49469bb482433259c207c1d
HirdayGupta <hirday.gupta@gmail.com>
2019-12-02 05:22:27 -0800

    Update gen_firebase_keys_from_envvar.sh

--- tradespace_backend/gen_firebase_keys_from_envvar.sh
+++ tradespace_backend/gen_firebase_keys_from_envvar.sh
@@ -1,9 +1,14 @@
 env_vars="type project_id private_key_id private_key client_email client_id auth_uri token_uri auth_provider_x509_cert_url client_x509_cert_url"
 
-echo "{"
+printf "{\n"
 for env_var in $env_vars
 do
     chosen_value=${!env_var}
-    echo "\"$env_var\": \"${chosen_value}\","
+    printf "\"$env_var\": \"${chosen_value}\""
+    if [ "$env_var" = "client_x509_cert_url" ]; then
+        printf "\n"
+    else
+        printf ",\n"
+    fi
 done
-echo "}"
+printf "}"
