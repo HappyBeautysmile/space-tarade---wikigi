dce7c2e8ac4cc3b51111493fe8e8d1d62f87b1b0
DarkBear0121 <darkbear0112@gmail.com>
2019-12-05 02:14:40 -0800

    add frontend test

--- tradespaceui/cypress/integration/home.spec.js
+++ tradespaceui/cypress/integration/home.spec.js
@@ -2,51 +2,121 @@
 
 context("Home", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:8080/#/");
-  });
+    cy.visit("https://tradespace.store/");
+    cy.get("div:nth-child(3) > button:nth-child(1)").click();
 
-  it("Register ", () => {
-    cy.get("button.v-btn:nth-child(4)").click();
-    cy.get(
-      "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__title > span"
-    ).contains("User Profile:");
-    cy.get(
-      "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > span"
-    ).click();
-    cy.get(
-      "#app > div.v-application--wrap > main > div > div > div > div > div:nth-child(4) > span"
-    ).contains("Every time you buy something");
+    cy.get("#input-24").type("daweihuang@g.ucla.edu");
+
+    cy.get("#input-27").type("aaaaaa");
+
+    cy.get("div.v-card__actions > button:nth-child(3)").click();
   });
 
   it("Search ", () => {
-    cy.get("#app > div > header > div > a:nth-child(3) > button").click();
-    cy.wait(100);
-    cy.get(".col")
-      .first()
-      .click();
-    cy.get("#app > div > main > div > div > div > li:nth-child(1)").click();
+    cy.get("header > div > a:nth-child(3) > button").click();
+
+    cy.get("#cover > form > div > div > input[type=text]").type("plaid");
+
+    cy.get("#s-cover > button").click();
+
+    cy.get("#myitem > div:nth-child(1)").click();
+
     cy.get(
-      "#app > div > main > div > div > div > div > div > div:nth-child(2) > div.card-body > p"
-    ).contains("Good shoes");
+      "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__title.headline.grey.lighten-2 > div > div.col > div.v-list-item__title"
+    ).contains("Plaid");
+
+    cy.get("div > div.v-card__actions > button:nth-child(3)").click();
   });
 
-  it("Account ", () => {
-    cy.get("#app > div > header > div > a:nth-child(4) > button").click();
+  it("Check if items component exists ", () => {
+    cy.get("header > div > a:nth-child(5) > button").click();
+
+    cy.get("div > a:nth-child(2) > button").click();
+
+    cy.get("div.row > li:nth-child(2)").click();
+
     cy.get(
-      "#app > div > main > div > div > div > div.v-avatar.v-list-item__avatar.grey > div > div.v-image__image.v-image__image--cover"
+      "div.v-card__title.headline.grey.lighten-2 > div > div.col > div.v-list-item__title"
+    ).contains("Skateboard");
+
+    cy.get("div.col > div:nth-child(2)").contains("Dawei Huang");
+
+    cy.get("div.col > div:nth-child(3)").contains("Los Angeles");
+
+    cy.get(
+      "div.v-avatar.v-list-item__avatar.grey > div > div.v-responsive__content"
+    );
+
+    cy.get("div.v-card__text > div > div:nth-child(1) > img");
+
+    cy.get("div.v-card__text > div > div:nth-child(2)").contains(
+      /(foobar|Hawk)/
     );
   });
 
-  it("Start Trade ", () => {
-    cy.get("#app > div > header > div > a:nth-child(5) > button").click();
-    cy.get("#__BVID__30").type("blue{enter}");
-    cy.get("#__BVID__30").type("adidas{enter}");
-    cy.get("#__BVID__30").type("ecofriendly{enter}");
+  it("Edit items ", () => {
+    cy.get("header > div > a:nth-child(5) > button").click();
+
+    cy.get("div > a:nth-child(2) > button").click();
+
+    cy.get("div.row > li:nth-child(2)").click();
+
+    cy.get("div.v-card__actions > button:nth-child(2) > span")
+      .wait(1000)
+      .click();
+
+    cy.get("#textarea")
+      .wait(1000)
+      .clear()
+      .type("foobar");
+
+    cy.get("div:nth-child(6) > button").click();
+
+    cy.get("header > div > a:nth-child(5) > button").click();
+
+    cy.get("div > a:nth-child(2) > button").click();
+
+    cy.get("div.row > li:nth-child(2)").click();
+
+    cy.get("div.v-card__text > div > div:nth-child(2)").contains("foobar");
+
     cy.get(
-      "#app > div > main > div > div > div:nth-child(4) > ul > button:nth-child(3)"
-    ).click();
+      "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2)"
+    )
+      .wait(3000)
+      .click();
+
+    cy.get("#textarea")
+      .wait(1000)
+      .clear()
+      .type("Tony Hawk used to own it.");
+  });
+
+  it("Trade Transaction ", () => {
+    cy.get("header > div > a:nth-child(3) > button").click();
+
+    cy.get("#cover > form > div > div > input[type=text]").type("tv");
+
+    cy.get("#s-cover > button").click();
+
+    cy.get("#myitem > div:nth-child(1)").click();
+
     cy.get(
-      "#app > div > main > div > div > div:nth-child(4) > ul > button"
-    ).should("have.length", 2);
+      "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__title.headline.grey.lighten-2 > div > div.col > div.v-list-item__title"
+    ).contains("TV");
+
+    cy.get("div > div.v-card__actions > button:nth-child(2)").click();
+
+    cy.get("header > div > a:nth-child(5) > button").click();
+
+    cy.get("#app > div > main > div > div > a:nth-child(3) > button")
+      .wait(2000)
+      .click();
+
+    cy.get("div.card > div:nth-child(2) > div:nth-child(2)");
+
+    cy.get(
+      "#app > div > main > div > div > div:nth-child(4) > div:nth-child(1) > div > div"
+    ).contains("Akaash");
   });
 });
