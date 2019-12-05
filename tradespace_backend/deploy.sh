94c7ffca888daa04490333e793f9317b2d538c8d
HirdayGupta <hirday.gupta@gmail.com>
2019-12-04 17:55:07 -0800

    fix multiline bash command

--- tradespace_backend/deploy.sh
+++ tradespace_backend/deploy.sh
@@ -7,11 +7,11 @@ pip install -r requirements.txt &&
 printf "killing old server processes\n" &&
 sudo ./kill_old_server.sh &&
 sudo ./.env/bin/gunicorn \
-    --bind 0.0.0.0:443 /
-    --certfile /etc/letsencrypt/live/tradespace.store/fullchain.pem /
-    --keyfile /etc/letsencrypt/live/tradespace.store/privkey.pem /
-    --access-logfile instance/gunicorn-access.log /
-    --error-logfile instance/gunicorn-error.log /
-    wsgi /
+    --bind 0.0.0.0:443 \
+    --certfile /etc/letsencrypt/live/tradespace.store/fullchain.pem \
+    --keyfile /etc/letsencrypt/live/tradespace.store/privkey.pem \
+    --access-logfile instance/gunicorn-access.log \
+    --error-logfile instance/gunicorn-error.log \
+    wsgi \
     --daemon &&
 printf "deploy successful!\n"
